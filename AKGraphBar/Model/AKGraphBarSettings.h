0aeeefa281b69ee46b942619831c19057f6e83e4
p0rokh <anton.corolev@academmedia.com>
2016-03-25 11:18:15 +0600

    add class GraphBarSettings and methods class: init, setup, getMax, getCurrentArray

--- AKGraphBar/Model/AKGraphBarSettings.h
+++ AKGraphBar/Model/AKGraphBarSettings.h
@@ -7,7 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface AKGraphBarSettings : NSObject
+
+@property (strong, nonatomic) UIColor* background;
+@property (strong, nonatomic) UIColor* columsLineColor;
+@property (strong, nonatomic) UIColor* bottomLineColor;
+@property (assign) CGFloat indentBottomLine;
+@property (assign) CGFloat indentTopLine;
+@property (assign) CGFloat indent;
+@property (assign) NSInteger numberColums;
+@property (assign) CGSize sizeMiniLine;
+@property (assign) CGFloat widthColums;
+@property (strong, nonatomic) NSArray* arrayData;
+@property (assign) BOOL miniLineHidden;
+@property (assign) CGFloat maxHeightColum;
+
+-(id)initDefaultWithArrayData:(NSArray*) arrayData;
+-(id)initDefaultWithArrayData:(NSArray*) arrayData andNumberColums:(NSInteger) numberColums;
+
 
 @end
