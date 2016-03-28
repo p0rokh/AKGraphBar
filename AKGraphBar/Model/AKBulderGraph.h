dfceda6fd0963d884dc25c8115fdfe0c7a54fdea
p0rokh <anton.corolev@academmedia.com>
2016-03-28 12:39:58 +0600

    add new class AKBuilderGraph

---
+++ AKGraphBar/Model/AKBulderGraph.h
@@ -0,0 +1,13 @@
+//
+//  AKBulderGraph.h
+//  AKGraphBar
+//
+//  Created by Anton Korolev on 28.03.16.
+//  Copyright © 2016 AntonKorolev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AKBulderGraph : NSObject
+
+@end
---
+++ AKGraphBar/Model/AKBulderGraph.m
@@ -0,0 +1,13 @@
+//
+//  AKBulderGraph.m
+//  AKGraphBar
+//
+//  Created by Anton Korolev on 28.03.16.
+//  Copyright © 2016 AntonKorolev. All rights reserved.
+//
+
+#import "AKBulderGraph.h"
+
+@implementation AKBulderGraph
+
+@end
