c4e217717b2897fb23642c822a30bc83bbf0344a
p0rokh <anton.corolev@academmedia.com>
2016-03-28 12:40:50 +0600

    done AKBuilderGraph

--- AKGraphBar/Model/AKBulderGraph.h
+++ AKGraphBar/Model/AKBulderGraph.h
@@ -7,7 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "AKGraphBar.h"
 
+/* v1.1.5 */
 @interface AKBulderGraph : NSObject
+
+/* Modify fields of graphics settings randomly and returns the unit after modification. */
+- (void) changeSettingsBarRandom: ( AKGraphBar * _Nonnull) graphBar /* completedBlock:(void (^ _Nonnull)(void))block */;
 
 @end
--- AKGraphBar/Model/AKBulderGraph.m
+++ AKGraphBar/Model/AKBulderGraph.m
@@ -10,4 +10,74 @@
 
 @implementation AKBulderGraph
 
+-(void)changeSettingsBarRandom:(AKGraphBar *)graphBar /* completedBlock:(void (^)(void))block */{
+    __weak typeof (self) weekSelf = self;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        typeof (self) strongSelf = weekSelf;
+        if (!strongSelf) {
+            return;
+        }
+        
+        @synchronized (strongSelf) {
+            [strongSelf update:graphBar /*completedBlock:block */];
+        }
+    });
+}
+
+- (void) update: ( AKGraphBar * ) graphBar /* completedBlock:(void (^ _Nonnull)(void))block */ {
+    NSInteger type = [self currentRandomFromMinimum:0 toMaximum:3];
+    NSUInteger count = [self currentRandomFromMinimum:3 toMaximum:14];
+    NSMutableArray* randomArray = [NSMutableArray arrayWithCapacity:count];
+    
+    for (int index = 0; index < count; index++) {
+        NSUInteger randomeNumber = [self currentRandomFromMinimum:150 toMaximum:500];
+        randomArray[index] = [NSNumber numberWithUnsignedInteger:randomeNumber];
+    }
+    
+    /*
+    @synchronized (graphBar) {
+        [graphBar setArrayData:randomArray];
+    }
+    */
+    [graphBar setArrayData:randomArray];
+    
+    switch (type) {
+        case 0:
+            [graphBar.settings setBackground:[self randomeColor]];
+            break;
+            
+        case 1:
+            [graphBar.settings setBottomLineColor:[self randomeColor]];
+            break;
+        case 2:
+            [graphBar.settings setColumsLineColor:[self randomeColor]];
+            break;
+            
+        default:
+            break;
+    }
+    
+    [graphBar drawGraphBar];
+}
+
+#pragma mark - random
+- (UIColor *) randomeColor {
+    NSUInteger random = [self currentRandomFromMinimum:0 toMaximum:3];
+    
+    if (random < 1) {
+        return [UIColor greenColor];
+    }else if (random < 2) {
+        return [UIColor redColor];
+    }else {
+        return [UIColor yellowColor];
+    }
+}
+
+- (NSUInteger) currentRandomFromMinimum:(NSUInteger) min toMaximum: (NSUInteger) max {
+    NSUInteger randomNum = 0;
+    arc4random_buf(&randomNum, sizeof(NSUInteger));
+    
+    return min + (randomNum % (max - min));
+}
+
 @end
